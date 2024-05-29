ee262c31f937fff6a0d8f7eceb6f622afa25f2c6
dlsiju <dlsiju@gmail.com>
2024-05-29 12:26:37 +0530

    alembic migration

--- DbConfiguration/ConnectDb.py
+++ DbConfiguration/ConnectDb.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL ="postgresql://postgres:postgres@localhost:5432/postgres"
 SQLALCHEMY_DATABASE_URL = DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
-Base = declarative_base()
